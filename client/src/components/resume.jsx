323295f9fe942c0ca294a4fd18978e985c03e8c6
Edgar Barrientos <edgarbarrientos@Edgars-MacBook-Pro.local>
2020-04-08 18:41:07 -0700

    mobile resume

--- client/src/components/resume.jsx
+++ client/src/components/resume.jsx
@@ -3,16 +3,25 @@ import {Link} from "react-router-dom";
 
 function Resume(){
     return (
-        <div className="container-fluid blk">
-            <div className="row">
-                <div className="col-12">
-                    <a href="resume.pdf" download>Download Resume</a>
+        <div>
+            <div className="container-fluid blk desktop-resume">
+                <div className="row ">
+                    <div className="col-12">
+                        <a href="resume.pdf" download>Download Resume</a>
+                    </div>
+                    <div className="col-12 center-align">
+                        <img className="center"src="resume.jpg"/>
+                    </div>
                 </div>
-                <div className="col-12 center-align">
-                    <img className="center"src="resume.jpg"/>
-                </div>
-                
             </div>
+            <embed className="mobile-resume"
+    src="resume.pdf"
+    type="application/pdf"
+    frameBorder="0"
+    scrolling="auto"
+    height="100%"
+    width="100%"
+></embed>
         </div>
     )
 }
