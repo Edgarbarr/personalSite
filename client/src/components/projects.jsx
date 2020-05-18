d671d4b15b8cab4fa486b76c8278a6230d2bfcb0
Edgar Barrientos <edgarbarrientos@Edgars-MacBook-Pro.local>
2020-05-17 20:03:38 -0700

    added hoh project

--- client/src/components/projects.jsx
+++ client/src/components/projects.jsx
@@ -4,25 +4,24 @@ function Projects(){
     return (
         <div className="container-fluid">
             <div className="row blk padding-50">
+                <img className="fadein col-12 col-md-8" src="hoh.png"/>
+                <div className="col-sm-8 col-md-4 margin-auto font-white fadein">
+                    <h2>Haven Of Happiness</h2>
+                    <p>React | HMTL5 | CSS3 | Firebase | Stripe</p>
+                    <p>An ecommerce app for handmade charms and keychains.</p>
+                    <p>WORK IN PROGRESS!</p>
+                    <p className="font-white">VISIT HERE: <a href="https://haven-of-happiness.herokuapp.com/" target="_blank">Haven Of Happiness</a></p>
+                </div>
+            </div>
+            <div className="row wte padding-50">
                 <video poster="loading.gif" className="fadein col-12 col-md-8" width="820"  autoPlay loop muted>
                     <source src="Trafalgar.mp4" type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
-                <div className="col-sm-8 col-md-4 margin-auto font-white fadein">
+                <div className="col-sm-8 col-md-4 margin-auto fadein">
                     <h2>Trafalgar Trip Detail Page Mockup</h2>
                     <p>React | HMTL5 | CSS3 | MongoDB | Express</p>
                     <p>An online webpage that details a trip of the clients choosing.</p>
-                </div>
-            </div>
-            <div className="row wte padding-50">
-                <video poster="loading.gif" className="fadein col-12 col-md-8" width="820" autoPlay loop muted>
-                    <source src="systemdesign.mp4" type="video/mp4"/>
-                    Your browser does not support the video tag.
-                </video>
-                <div className="col-sm-4 col-md-4 margin-auto fadein">
-                    <h2>REI System Design</h2>
-                    <p>PostgreSQL | MongoDB | K6 | AWS EC2 | Jest | Docker</p>
-                    <p>Scaled and deployed a back end for an outdoor clothing app store.</p>
                 </div>
             </div>
             <div className="row blk padding-50">
@@ -34,6 +33,17 @@ function Projects(){
                     <h2>Trivia Game</h2>
                     <p> React | PostgreSQL | Express | Javascript | Socket.io</p>
                     <p>Trivia Game is an app that utilizes websockets get a real time response between users in a chat room. The users can participate in a trivia game with a theme of their choice.</p>
+                </div>
+            </div>
+            <div className="row wte padding-50">
+                <video poster="loading.gif" className="fadein col-12 col-md-8" width="820" autoPlay loop muted>
+                    <source src="systemdesign.mp4" type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+                <div className="col-sm-4 col-md-4 margin-auto fadein">
+                    <h2>REI System Design</h2>
+                    <p>PostgreSQL | MongoDB | K6 | AWS EC2 | Jest | Docker</p>
+                    <p>Scaled and deployed a back end for an outdoor clothing app store.</p>
                 </div>
             </div>
         </div>
