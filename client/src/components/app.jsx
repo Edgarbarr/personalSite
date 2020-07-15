0d51092678ef9e913d66db060de116e8757116db
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-07-14 19:55:07 -0700

    added loading

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./navbar/navBar.jsx";
+import Spinner from "./with-spinner/with-spinner.component.jsx";
 // import Home from "./home/home.jsx";
 // import Projects from "./projects/projects.jsx";
 // import Resume from "./resume.jsx";
@@ -8,15 +9,29 @@ import NavBar from "./navbar/navBar.jsx";
 // import Footer from "./footer/footer.jsx";
 import loadable from '@loadable/component'
 
-const Projects = loadable(() => import("./projects/projects.jsx"));
-const Resume = loadable(() => import("./resume.jsx"));
-const Contact = loadable(() => import("./contact.jsx"));
-const Home = loadable(() => import("./home/home.jsx"));
-const Footer = loadable(() => import("./footer/footer.jsx"));
+const Projects = loadable(() => import("./projects/projects.jsx"), {
+  fallback: <Spinner/>
+});
+const Resume = loadable(() => import("./resume.jsx"), {
+  fallback: <Spinner/>
+});
+const Contact = loadable(() => import("./contact.jsx"), {
+  fallback: <Spinner/>
+});
+const Home = loadable(() => import("./home/home.jsx"), {
+  fallback: <Spinner/>
+});
+const Footer = loadable(() => import("./footer/footer.jsx"), {
+  fallback: <Spinner/>
+});
 
 
 
 function App() {
+  var [loading, setLoading] = useState(false);
+  useEffect(() => {
+    console.log(Projects)
+  },[Projects, Resume, Contact, Home])
   return (
     <div className="soItBegins">
       <NavBar />
