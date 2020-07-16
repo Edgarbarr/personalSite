20f53bced60463863dc2513d7bf7b3c6a7172fb6
Edgar Barrientos <edgarbarrientos@Edgars-MBP.lan>
2020-07-15 17:57:47 -0700

    fix

--- client/src/components/home/home.styled.jsx
+++ client/src/components/home/home.styled.jsx
@@ -221,8 +221,8 @@ export const ImageContainer = styled.div`
         background-size: cover;
         background-attachment: fixed;
         z-index:-1;
-        width:100%;
-        height: 600px;
+        width:100vw;
+        height: 100vh;
         overflow:hidden;
         top: 0; 
         left: 0; 
